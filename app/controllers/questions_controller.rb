742d67e875707cfd125e1c21f846d71125b41b07
carole valentin <valentincarole25@gmail.com>
2021-08-09 14:14:52 +0200

    stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,13 +6,13 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
 
-    if @question == 'I am going to work'
-      @answer = 'Great!'
+  if @question == 'I am going to work'
+    @answer = 'Great!'
     elsif @question == '?'
       @answer = "Silly question, get dressed and go to work!"
-    else
+     else
       @answer = "I don't care, get dressed and go to work!"
-    end
+  end
 
   end
 
